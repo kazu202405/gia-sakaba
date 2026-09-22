37e0918f32cb93d080a310a3a5352998c3b6b611
kazu202405 <global.information.academy@gmail.com>
2026-09-22 20:34:02 +0900

    Allow live quest edit and applicant routes

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -61,7 +61,7 @@ export async function updateSession(request: NextRequest) {
     pathname === "/guild/master" ||
     pathname === "/guild/master/gathering/new" ||
     /^\/guild\/projects\/[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}(?:\/edit)?$/i.test(pathname) ||
-    /^\/guild\/quests\/[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
+    /^\/guild\/quests\/[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}(?:\/(?:edit|applicants))?$/i.test(
       pathname,
     );
 
